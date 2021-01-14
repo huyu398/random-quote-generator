586025b8fcb699be5cffc4a567fafee6b3ef5a78
huyu398 <huyu.sakuya4645@gmail.com>
2021-01-15 06:28:51 +0900

    Quote の更新ができるボタンを追加

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import { useState, useEffect } from 'react';
 import { Container } from '@material-ui/core';
 import axios from 'axios';
 
+import Header from './Header.js'
 import Quote from './Quote.js'
 
 function App() {
   const [quote, setQuote] = useState('');
 
-  useEffect(() => {
+  const refreshQuote = () => {
     axios.get('https://quote-garden.herokuapp.com/api/v3/quotes/random')
       .then(res => {
         const quoteText = res.data.data[0].quoteText;
         setQuote(quoteText);
       });
+  };
+
+  useEffect(() => {
+    refreshQuote();
   }, []);
 
   return (
-    <Container maxWidth='sm'>
-      <Quote text={quote}/>
+    <Container maxWidth='lg'>
+      <Header handleRefresh={refreshQuote}/>
+      <Container maxWidth='sm'>
+        <Quote text={quote}/>
+      </Container>
     </Container>
   )
 }
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import { Box, Button } from '@material-ui/core'
+import { Refresh } from '@material-ui/icons'
+
+function Header(props) {
+  return (
+    <header>
+      <Box display="flex" justifyContent="flex-end" my={3}>
+        <Button endIcon={<Refresh/>} style={{textTransform: 'none'}} onClick={() => { props.handleRefresh() }}>
+          random
+        </Button>
+      </Box>
+    </header>
+  )
+}
+
+export default Header;
