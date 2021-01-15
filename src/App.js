645eb463683826b048d1f8177256d57f8b7239b1
huyu398 <huyu.sakuya4645@gmail.com>
2021-01-15 10:57:09 +0900

    react-router-dom を使ってルーティングを設定

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { useState, useEffect } from 'react';
-import { Container } from '@material-ui/core';
-import axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import Header from './Header.js'
-import Quote from './Quote/Quote.js'
+import RandomQuote from './RandomQuote.js';
 
 function App() {
-  const [quote, setQuote] = useState({});
-
-  const refreshQuote = () => {
-    axios.get('https://quote-garden.herokuapp.com/api/v3/quotes/random')
-      .then(res => {
-        const quote = res.data.data[0];
-        setQuote(quote);
-      });
-  };
-
-  useEffect(() => {
-    refreshQuote();
-  }, []);
-
   return (
-    <Container maxWidth='lg'>
-      <Header handleRefresh={refreshQuote}/>
-      <Quote data={quote}/>
-    </Container>
+    <BrowserRouter>
+      <Route exact path='/' component={RandomQuote} />
+    </BrowserRouter>
   )
 }
 
---
+++ src/RandomQuote.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { Container } from '@material-ui/core';
+import axios from 'axios';
+
+import Header from './Header.js'
+import Quote from './Quote/Quote.js'
+
+function RandomQuote() {
+  const [quote, setQuote] = useState({});
+
+  const refreshQuote = () => {
+    axios.get('https://quote-garden.herokuapp.com/api/v3/quotes/random')
+      .then(res => {
+        const quote = res.data.data[0];
+        setQuote(quote);
+      });
+  };
+
+  useEffect(() => {
+    refreshQuote();
+  }, []);
+
+  return (
+    <Container maxWidth='lg'>
+      <Header handleRefresh={refreshQuote}/>
+      <Quote data={quote}/>
+    </Container>
+  )
+}
+
+export default RandomQuote;
