9fb9c99d8f4c4ed78b08379466c4aea0db2f4e2b
huyu398 <huyu.sakuya4645@gmail.com>
2021-01-15 10:27:41 +0900

    QuoteAuthor の見た目を調整

--- src/Quote/QuoteAuthor.js
+++ src/Quote/QuoteAuthor.js
@@ -1,7 +1,17 @@
-import { Container, Typography } from '@material-ui/core';
+import { Container, Grid, Typography } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
+import { ArrowForward } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => createStyles({
+  authorCard: {
+    paddingTop:    `${theme.spacing(5)}px`,
+    paddingBottom: `${theme.spacing(5)}px`,
+    '&:hover': {
+      color: 'white',
+      background: grey[900],
+    }
+  },
   quoteAuthor: {
     fontWeight: 'bold'
   }
@@ -11,13 +21,20 @@ function QuoteAuthor(props) {
   const classes = useStyles();
 
   return (
-    <Container maxWidth='sm'>
-      <Typography variant="h4" className={classes.quoteAuthor}>
-        { props.author }
-      </Typography>
-      <Typography variant="caption">
-        { props.genre }
-      </Typography>
+    <Container maxWidth="sm" className={classes.authorCard}>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <Typography variant="h4" className={classes.quoteAuthor}>
+            { props.author }
+          </Typography>
+          <Typography variant="caption">
+            { props.genre }
+          </Typography>
+        </Grid>
+        <Grid>
+          <ArrowForward style={{color: 'white'}}/>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
